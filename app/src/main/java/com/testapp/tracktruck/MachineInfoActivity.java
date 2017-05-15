48c07bf73634d373f5e1aaadc9756c294d945848
PaulStets <stetsenko.pavlo@gmail.com>
2017-05-15 10:14:44 +0300

    Added distance calculation

--- app/src/main/java/com/testapp/tracktruck/MachineInfoActivity.java
+++ app/src/main/java/com/testapp/tracktruck/MachineInfoActivity.java
@@ -22,24 +22,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.testapp.tracktruck.utils.DirectionsJSONParser;
 
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import static android.R.attr.id;
-import static com.testapp.tracktruck.R.id.map;
 
 public class MachineInfoActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -55,6 +42,8 @@ public class MachineInfoActivity extends AppCompatActivity implements OnMapReady
     private String workingTime;
     private DatabaseReference mDatabase;
     private String userID;
+
+    private List<LatLng> allLatLng;
 
     private TextView id;
     private TextView coordinates;
@@ -161,10 +150,13 @@ public class MachineInfoActivity extends AppCompatActivity implements OnMapReady
                             DataSnapshot first = log.next();
                             origin = new LatLng(first.child("latitude").getValue(Double.class),
                                     first.child("longitude").getValue(Double.class));
+                            allLatLng = new ArrayList<LatLng>();
+                            allLatLng.add(origin);
                             while (log.hasNext()) {
                                 DataSnapshot last = log.next();
                                 dest = new LatLng(last.child("latitude").getValue(Double.class),
                                         last.child("longitude").getValue(Double.class));
+                                allLatLng.add(dest);
                             }
 
 
@@ -174,11 +166,13 @@ public class MachineInfoActivity extends AppCompatActivity implements OnMapReady
 
                 }
                 if(origin != null && dest != null) {
-                    String url = getDirectionsUrl(origin,dest);
-                    FetchUrl FetchUrl = new FetchUrl();
-                    FetchUrl.execute(url);
                     if(mMap != null) {
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origin, 8));
+                        PolylineOptions lineOptions = new PolylineOptions();
+                        lineOptions.addAll(allLatLng);
+                        lineOptions.width(8f);
+                        lineOptions.color(R.color.wallet_holo_blue_light);
+                        mMap.addPolyline(lineOptions);
                     }
 
                     float[] results = new float[1];
@@ -230,169 +224,6 @@ public class MachineInfoActivity extends AppCompatActivity implements OnMapReady
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-    }
-
-    // Fetches data from url passed
-    private class FetchUrl extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... url) {
-
-            // For storing data from web service
-            String data = "";
-
-            try {
-                // Fetching the data from web service
-                data = downloadUrl(url[0]);
-                Log.d("Background Task data", data.toString());
-            } catch (Exception e) {
-                Log.d("Background Task", e.toString());
-            }
-            return data;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            ParserTask parserTask = new ParserTask();
-
-            // Invokes the thread for parsing the JSON data
-            parserTask.execute(result);
-
-        }
-    }
-
-    private String getDirectionsUrl(LatLng origin, LatLng dest) {
-
-        // Origin of route
-        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
-
-        // Destination of route
-        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
-
-        // Sensor enabled
-        String sensor = "sensor=false";
-
-        // Building the parameters to the web service
-        String parameters = str_origin + "&" + str_dest + "&" + sensor;
-
-        // Output format
-        String output = "json";
-
-        // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
-
-        return url;
-    }
-
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL(strUrl);
-
-            // Creating an http connection to communicate with url
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            // Connecting to url
-            urlConnection.connect();
-
-            // Reading data from url
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuffer sb = new StringBuffer();
-
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-            data = sb.toString();
-            Log.d("downloadUrl", data.toString());
-            br.close();
-
-        } catch (Exception e) {
-            Log.d("Exception", e.toString());
-        } finally {
-            iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
-
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
-
-        // Parsing the data in non-ui thread
-        @Override
-        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-
-            JSONObject jObject;
-            List<List<HashMap<String, String>>> routes = null;
-
-            try {
-                jObject = new JSONObject(jsonData[0]);
-                Log.d("ParserTask",jsonData[0].toString());
-                DirectionsJSONParser parser = new DirectionsJSONParser();
-                Log.d("ParserTask", parser.toString());
-
-                // Starts parsing data
-                routes = parser.parse(jObject);
-                Log.d("ParserTask","Executing routes");
-                Log.d("ParserTask",routes.toString());
-
-            } catch (Exception e) {
-                Log.d("ParserTask",e.toString());
-                e.printStackTrace();
-            }
-            return routes;
-        }
-
-        // Executes in UI thread, after the parsing process
-        @Override
-        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-            ArrayList<LatLng> points;
-            PolylineOptions lineOptions = null;
-
-            // Traversing through all the routes
-            for (int i = 0; i < result.size(); i++) {
-                points = new ArrayList<>();
-                lineOptions = new PolylineOptions();
-
-                // Fetching i-th route
-                List<HashMap<String, String>> path = result.get(i);
-
-                // Fetching all the points in i-th route
-                for (int j = 0; j < path.size(); j++) {
-                    HashMap<String, String> point = path.get(j);
-
-                    double lat = Double.parseDouble(point.get("lat"));
-                    double lng = Double.parseDouble(point.get("lng"));
-                    LatLng position = new LatLng(lat, lng);
-
-                    points.add(position);
-                }
-
-                // Adding all the points in the route to LineOptions
-                lineOptions.addAll(points);
-                lineOptions.width(8);
-                lineOptions.color(R.color.wallet_holo_blue_light);
-
-                Log.d("onPostExecute","onPostExecute lineoptions decoded");
-
-            }
-
-            // Drawing polyline in the Google Map for the i-th route
-            if(lineOptions != null) {
-                mMap.addPolyline(lineOptions);
-            }
-            else {
-                Log.d("onPostExecute","without Polylines drawn");
-            }
-        }
     }
 
 }
